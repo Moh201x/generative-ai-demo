c4044679b8aaeb2fdb380e5d8af559a4ba188631
Mohammad <mohammedk.daoud360@gmail.com>
2023-07-25 02:04:09 +0300

    get key

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import requests
 import learn
 import os
 from dotenv import load_dotenv
+import google.auth
+from google.auth.transport.requests import Request
 
 
 app = Flask(__name__)
@@ -21,6 +23,17 @@ def extract_sentences(api_response):
 
     return [website_ad, instagram_ad]
 
+
+def getKey():
+    API_ENDPOINT = "us-central1-aiplatform.googleapis.com"
+    GOOGLE_KEY = os.environ.get("GOOGLE_KEY")
+    URL = f"https://{API_ENDPOINT}/v1/projects/{GOOGLE_KEY}/locations/us-central1/publishers/google/models/chat-bison@001:predict"
+    creds, _ = google.auth.default(scopes=["https://www.googleapis.com/auth/cloud-platform"])
+    creds.refresh(Request())
+    return creds.token
+
+    
+
 def makeApiCall(type,des):
 
     API_ENDPOINT = "us-central1-aiplatform.googleapis.com"
@@ -30,7 +43,7 @@ def makeApiCall(type,des):
     url = f"https://{API_ENDPOINT}/v1/projects/{PROJECT_ID}/locations/us-central1/publishers/google/models/{MODEL_ID}:predict"
 
     headers = {
-        "Authorization": f"Bearer {os.getenv('KEY')}",
+        "Authorization": f"Bearer {getKey()}",
         "Content-Type": "application/json",
         "lifetime": "LIFETIME"
 
@@ -43,7 +56,7 @@ def makeApiCall(type,des):
             }
         ],
         "parameters": {
-            "temperature": 0.7,
+            "temperature": 0.9,
             "maxOutputTokens": 700,
             "topP": 0.8,
             "topK": 40
@@ -60,7 +73,7 @@ def makeApiCall(type,des):
         print(f"Request failed with status code: {response.status_code}")
         print("Error message:")
         print(response.text) 
-        return '404'
+        return '401'
 
 @app.route('/ai')
 def homePage():
@@ -69,13 +82,11 @@ def homePage():
     
 @app.route('/info',methods=['POST'])
 def display_data():
-   content= makeApiCall(request.form.get('ctype'),request.form.get('cdes'))['predictions'][0]['content']
-   if(content=='this is not cloth type'):
-    return render_template('error.html')
-   else:    
-    return render_template('secondPage.html',data=extract_sentences(content))
-
-    
+    content= makeApiCall(request.form.get('ctype'),request.form.get('cdes'))['predictions'][0]['content']
+    if(content=='this is not cloth type'):
+        return render_template('error.html')
+    else:    
+        return render_template('secondPage.html',data=extract_sentences(content))
 
 if __name__ == '__main__':
     app.run()
